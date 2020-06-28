d3c7c7c965ee3655c18e40697d6d3094d713dfba
AmauriPruebas <Nikobellik1516>
2020-06-28 02:10:15 -0500

    Slider yes yes

--- js/SideNavPush.js
+++ js/SideNavPush.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $(".SideNavBott").click(function() {
         if ($("#SideNavRight").width() == 0) {
-            document.getElementById("SideNavRight").style.width = "350px";
-            document.getElementById("contentmain").style.paddingRight = "350px";
-            document.getElementById("SideNavBott").style.marginRight = "350px";
+            document.getElementById("SideNavRight").style.width = "415px";
+            document.getElementById("contentmain").style.paddingRight = "415px";
+            document.getElementById("SideNavBott").style.marginRight = "415px";
         } else {
             document.getElementById("SideNavRight").style.width = "0";
             document.getElementById("contentmain").style.paddingRight = "0";
